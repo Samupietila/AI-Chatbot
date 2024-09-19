aba455fec649bba1c9295a321b8ab98784f14b58
zehra <Zehrag@metropolia.fi>
2024-09-19 17:08:19 +0300

    Päivitetty actions.py, nlu.yml, stories.yml ja domain.yml

--- actions/actions.py
+++ actions/actions.py
@@ -1,10 +1,20 @@
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import SessionStarted, ActionExecuted
+from rasa_sdk.events import SlotSet
+
 
 class ActionBeforeListen(Action):
     def name(self):
         return "action_before_listen"
-    async def run(self, dispatcher, tracker, domain):
+
+    async def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: dict) -> list:
+
         last_action_name = tracker.latest_action_name
-        return [SlotSet("before_listen", last_action_name)]
+
+
+        bonus_info = tracker.get_slot("bonus_info")
+
+        return [
+            SlotSet("before_listen", last_action_name),
+
+        ]
