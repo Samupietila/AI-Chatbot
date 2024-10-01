c352dcc4c18c96348a7325f491319207d61fc565
Samu <samupie@metropolia.fi>
2024-10-01 12:30:19 +0300

    Nothing changed

--- actions/actions.py
+++ actions/actions.py
@@ -1,6 +1,11 @@
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SessionStarted, ActionExecuted
+import logging
+print("Imported logging")
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+print("Set logging level to DEBUG")
 
 
 
@@ -10,4 +15,6 @@ class ActionBeforeListen(Action):
         return "action_before_listen"
     async def run(self, dispatcher, tracker, domain):
         last_action_name = tracker.latest_action_name
+        logger.debug(f"Last action name: {last_action_name}")
+        logger.debug(f"Tracker: {tracker}")
         return [SlotSet("before_listen", last_action_name)]
