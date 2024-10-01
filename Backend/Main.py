6681f22d22c22d2ff02b115e3000b34eb2f19fee
dogface3 <samuel.nousiainen@metropolia.fi>
2024-10-01 10:29:08 +0300

    db connection and authentication skeleton

--- Backend/Main.py
+++ Backend/Main.py
@@ -1,9 +1,10 @@
 from DbConnection import db_connection
 from authentication import authenticate_user
+from registration import register
 
 def main():
+    register()
     authenticate_user()
 
 if __name__ == "__main__":
     main()
-
