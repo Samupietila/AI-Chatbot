1d423744eb1b4da557d63c93a1cd7070d2992572
Mika <Mika.Gronroos2@metropolia.fi>
2024-10-27 18:43:52 +0200

    Translations Yupdate

--- Flask-Website/website/__init__.py
+++ Flask-Website/website/__init__.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 from flask_login import LoginManager, current_user
+from flask_babel import Babel, gettext as _
 
+def get_locale():
+        language = request.cookies.get('language')
+        if language:
+            return language
+        return request.accept_languages.best_match(['en', 'fr', 'fi'])
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '123SecretKey321'
+    app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+    app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
     
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
@@ -26,7 +34,10 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     
-
+    babel = Babel(app)
+    
+    
+    
+    babel.init_app(app, locale_selector=get_locale)
     
     return app
-    
